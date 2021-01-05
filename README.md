85fa20f7f5cc178592898736b1ec25fc7e08216d
Noha Abuaesh <noha.abuaesh@gmail.com>
2021-01-05 16:05:42 +0300

    completed

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
 # Pixel Art Maker Project
 
-## Table of Contents
+##This is a single-page web app that allows users to draw pixel art on a customizable canvas!
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
+The interface is built using HTML and CSS. JavaScript code is used to let the user create a grid of squares representing their design, and apply colors to those squares to create a digital masterpiece!
 
-## Instructions
+Users are be able to:
 
-To get started, open `designs.js` and start building out the app's functionality.
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
-
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+- Dynamically set the size of the table as an N by M grid.
+- Choose a color.
+- Click a cell in the grid to fill that cell with the chosen color.
--- designs.js
+++ designs.js
@@ -1,10 +1,47 @@
-// Select color input
 // Select size input
-
 // When size is submitted by the user, call makeGrid()
+document.querySelector('#submit')
+    .addEventListener('click', 
+        function(e){
+            e.preventDefault(); // To stop the page from refreshing
+            makeGrid();
+        });
 
 function makeGrid() {
+    clearGrid(); // To start with a clean table
+    //Construct the grid:
+    let w = document.querySelector('#inputWidth').value;
+    let h = document.querySelector('#inputHeight').value;
+    let txt ='';
+    for(var i=0; i<h; i++)
+    {
+        txt += "<tr>"
+        for(var j=0; j<w;j++)
+            txt += "<td class='canvasCell' style='background-color: #ffffff;'>  </td>";
+        
+        txt += "</tr>"
+    }
+    document.querySelector('#pixelCanvas')
+        .insertAdjacentHTML('afterbegin', txt);
+  
+    //Start painting:
+    paint();
+}
 
-// Your code goes here!
+function clearGrid(){
+        document.querySelectorAll('.canvasCell').forEach(function(t){
+            t.remove();
+        });
+}
 
+function paint()
+{
+
+    document.querySelector('#pixelCanvas')
+    .addEventListener('click', function(e){
+        //Update color setting:
+        var color = document.querySelector('#colorPicker').value;
+        //Paint the cell with the selected color
+        e.target.style = `background-color: ${color}`;
+    });
 }
